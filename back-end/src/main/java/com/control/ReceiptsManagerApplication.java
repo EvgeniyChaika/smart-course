cb9ca2c1b3479c9b09344301f3d9cc4fe9891e85
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-03-08 14:32:51 +0200

    Configuring maven modules architecture for war building

---
+++ back-end/src/main/java/com/control/ReceiptsManagerApplication.java
@@ -0,0 +1,16 @@
+package com.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by evgeniy on 08.03.17.
+ */
+
+@SpringBootApplication
+public class ReceiptsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptsManagerApplication.class, args);
+    }
+}

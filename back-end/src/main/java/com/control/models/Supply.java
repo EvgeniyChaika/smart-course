59f9caaed7e5400083f093c1ec7089c286aeab1d
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-03-08 18:28:41 +0200

    Changed model Supply, sql table's schemas

--- back-end/src/main/java/com/control/models/Supply.java
+++ back-end/src/main/java/com/control/models/Supply.java
@@ -14,7 +14,7 @@ public class Supply {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(name = "date")
     @Temporal(TemporalType.DATE)
@@ -26,11 +26,11 @@ public class Supply {
     @OneToMany(mappedBy = "supply")
     private Set<SupplyDetail> supplyDetails;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
--- back-end/src/main/resources/mysql/tables.sql
+++ back-end/src/main/resources/mysql/tables.sql
@@ -1,51 +1,51 @@
 CREATE TABLE receipts.rNetworks
 (
-  id INT PRIMARY KEY NOT NULL,
+  id      INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   network VARCHAR(255)
 );
 
 
 CREATE TABLE receipts.rPairings
 (
-  id INT PRIMARY KEY NOT NULL,
-  date DATETIME,
-  last_pairing_id INT,
+  id                INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  date              DATETIME,
+  last_pairing_id   INT,
   pairing_requested VARCHAR(255)
 );
 
 CREATE TABLE receipts.rState
 (
-  id INT PRIMARY KEY NOT NULL,
+  id          INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   description VARCHAR(255),
-  state VARCHAR(255)
+  state       VARCHAR(255)
 );
 
 CREATE TABLE receipts.rSupplies
 (
-  id NUMERIC(19) PRIMARY KEY NOT NULL,
-  date DATETIME,
-  network_id INT,
-  CONSTRAINT FKibsfw1mao1wlu4vr8ckjshmh1 FOREIGN KEY (network_id) REFERENCES receipts.rNetworks (id)
+  id         INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  date       DATETIME,
+  network_id INTEGER,
+  CONSTRAINT network_id FOREIGN KEY (network_id) REFERENCES receipts.rNetworks (id)
 );
 
 CREATE TABLE receipts.rSuppliesDetails
 (
-  id INT PRIMARY KEY NOT NULL,
-  supply_id NUMERIC(19),
-  voucher_id INT,
-  CONSTRAINT FKk1hlkg4hc0ijrerg6hu3503co FOREIGN KEY (supply_id) REFERENCES receipts.rSupplies (id),
-  CONSTRAINT FKikngi25q6podaxwtie2afjpq1 FOREIGN KEY (voucher_id) REFERENCES receipts.rVoucher (id)
+  id         INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  supply_id  INTEGER,
+  voucher_id INTEGER,
+  CONSTRAINT supply_id FOREIGN KEY (supply_id) REFERENCES receipts.rSupplies (id),
+  CONSTRAINT voucher_id FOREIGN KEY (voucher_id) REFERENCES receipts.rVoucher (id)
 );
 
 CREATE TABLE receipts.rVoucher
 (
-  id INT PRIMARY KEY NOT NULL,
-  amount FLOAT,
-  created_on DATETIME,
-  expired_on DATETIME,
-  pin VARCHAR(255),
-  state_id INT,
-  supplyDetail_id INT,
-  CONSTRAINT FKji4cstebopkyhs3k2pvukq60q FOREIGN KEY (state_id) REFERENCES receipts.rState (id),
-  CONSTRAINT FK36ht4iq8v8jc9qusmtqcukiwg FOREIGN KEY (supplyDetail_id) REFERENCES receipts.rSuppliesDetails (id)
+  id              INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  amount          FLOAT,
+  created_on      DATETIME,
+  expired_on      DATETIME,
+  pin             VARCHAR(255),
+  state_id        INTEGER,
+  supplyDetail_id INTEGER,
+  CONSTRAINT state_id FOREIGN KEY (state_id) REFERENCES receipts.rState (id),
+  CONSTRAINT supplyDetail_id FOREIGN KEY (supplyDetail_id) REFERENCES receipts.rSuppliesDetails (id)
 );

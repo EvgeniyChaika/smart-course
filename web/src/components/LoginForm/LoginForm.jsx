40ff784db7f090c2a71face4a39a1b9947e33c00
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-04-12 11:33:42 +0300

    changed login form set state

--- web/src/components/LoginForm/LoginForm.jsx
+++ web/src/components/LoginForm/LoginForm.jsx
@@ -5,6 +5,27 @@ export default class LoginForm extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            login: "",
+            password: ""
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange() {
+        this.setState(
+            {
+                login: this.loginInput.value,
+                password: this.passwordInput.value
+            }
+        );
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+        console.log(event);
     }
 
     render() {
@@ -15,24 +36,27 @@ export default class LoginForm extends Component {
                         <div className="row">
                             <div className="col-sm-4 col-sm-offset-4">
                                 <h2 className="text-center">Receipts Manager</h2>
-                                <form role="form">
+                                <form role="form" onSubmit={this.handleSubmit} onChange={this.handleInputChange}>
                                     <div className="form-group">
-                                        <label htmlFor="exampleInputEmail1">Email address</label>
-                                        <input type="email" className="form-control" id="exampleInputEmail1"
-                                               placeholder="Enter email"/>
+                                        <label htmlFor="exampleInputEmail1">Login :</label>
+                                        <input type="text" className="form-control" id="exampleInputEmail1"
+                                               ref={(input) => this.loginInput = input}
+                                               placeholder="Login"/>
                                     </div>
                                     <div className="form-group">
-                                        <label htmlFor="exampleInputPassword1">Password</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1"
+                                        <label htmlFor="exampleInputPassword1">Password :</label>
+                                        <input type="text" className="form-control" id="exampleInputPassword1"
+                                               ref={(input) => this.passwordInput = input}
                                                placeholder="Password"/>
                                     </div>
-
                                     <div className="checkbox">
                                         <label>
                                             <input type="checkbox"/>Remember me
                                         </label>
                                     </div>
-                                    <button type="submit" className="btn btn-default">Submit</button>
+                                    <div>
+                                        <input type="submit" className="btn btn-default" value="Submit"/>
+                                    </div>
                                 </form>
                             </div>
                         </div>

2c33046275b99c3d2c45537887af768e7d0e0080
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-08-16 18:17:40 +0300

    User dao model

--- back-end/src/main/java/com/course/SmartCourseApplication.java
+++ back-end/src/main/java/com/course/SmartCourseApplication.java
@@ -1,11 +1,15 @@
 package com.course;
 
+import com.course.models.User;
+import com.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 /**
  * Created by evgeniy on 08.03.17.
@@ -28,20 +32,16 @@ public class SmartCourseApplication extends SpringBootServletInitializer impleme
 
     private void checkNetworksTable() {
 
-//        NetworkRepository networkRepository = applicationContext.getBean(NetworkRepository.class);
-//
-//        List<User> networksList = networkRepository.findAll();
-//
-//        if (networksList.isEmpty()) {
-//            User network1 = new User();
-//            network1.setNetwork("UkrTelecom");
-//            networksList.add(network1);
-//
-//            User network2 = new User();
-//            network2.setNetwork("Triolan");
-//            networksList.add(network2);
-//
-//            networksList = networkRepository.save(networksList);
-//        }
+        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
+
+        List<User> userList = userRepository.findAll();
+
+        if (userList.isEmpty()) {
+            User userAdmin = new User();
+            userAdmin.setUserName("Admin");
+            userList.add(userAdmin);
+
+            userList = userRepository.save(userList);
+        }
     }
 }
--- back-end/src/main/java/com/course/models/User.java
+++ back-end/src/main/java/com/course/models/User.java
@@ -10,4 +10,34 @@ import javax.persistence.*;
 @Table(name = "sc_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
 }
